828472ed2392146586d952520ebb3a2a6d9d699c
Joseph Reeve <joseph.reeve@googlemail.com>
2018-06-14 22:45:59 +0100

    Logging

--- index.js
+++ index.js
@@ -71,9 +71,9 @@ async function getLatestDownloadedGfsRunStep(runCode) {
         .filter(file => file.slice(-5).match(/\.f[0-9]+/))
         .map(file => file.split('.').slice(-1)[0].slice(1))
         .map(stepHour => parseInt(stepHour))
-        .map(a => console.log('value', a) || a)
+        .map(a => console.log('value', typeof a) || a)
         .sort()
-        .map(a => console.log('value', a) || a)
+        .map(a => console.log('value', typeof a) || a)
         .reverse()[0];
     return latestDownloadedStep;
 }
