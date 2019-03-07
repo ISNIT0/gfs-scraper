37153885e92e2db1f4d3985a6ad2997bdc409066
Joe Reeve <joseph.reeve@googlemail.com>
2019-03-07 15:51:17 +0100

    :green_heart: Making build and compilation improvements

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+rm -rf bin
 tsc
-echo "#!/usr/bin/env node"|cat - ./bin/index.js > /tmp/gfsscraper && mv /tmp/gfsscraper ./bin/gfsscraper
+echo "#!/usr/bin/env node" | cat - ./bin/index.js > /tmp/gfsscraper && mv /tmp/gfsscraper ./bin/gfsscraper
