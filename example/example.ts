8792e071275070f3932626f1036414e3f4ba9137
Joe Reeve <joseph.reeve@googlemail.com>
2019-03-07 18:19:24 +0100

    :bulb: Fixed lint errors in example

--- example/example.ts
+++ example/example.ts
@@ -6,14 +6,14 @@ const outFile = join(__dirname, 'out.grib2');
 gfsScraper.downloadStep(outFile, '2018082712', '000', 'all');
 
 gfsScraper.getLatestRun()
-    .then(run => {
+    .then((run) => {
         console.info(`Latest available GFS run is [${run}]`);
     });
 
 gfsScraper.downloadStep('./out.grib2', '2018082712', '000', [{
     height: '2maboveground',
-    parameter: 'TMP'
+    parameter: 'TMP',
 }, {
     height: 'surface',
-    parameter: 'LAND'
+    parameter: 'LAND',
 }]);
