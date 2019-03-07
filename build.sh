b656df641ab2e93f5d661eb1fd3ff5a2642b9fd7
Joe Reeve <joseph.reeve@googlemail.com>
2019-03-07 17:36:59 +0100

    :construction_worker: More build changes

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 rm -rf bin
-tsc
+tsc --build tsconfig.build.json
 echo "#!/usr/bin/env node" | cat - ./bin/index.js > /tmp/gfsscraper && mv /tmp/gfsscraper ./bin/gfsscraper
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import * as yargs from 'yargs';
-import * as moment from 'moment';
+import moment from 'moment';
 
 import {
     getLatestAvailableGfsRun,
